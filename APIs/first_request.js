c4fd6b1db4d125faa019e56f9e01d2d6d1c9cb24
VineetPrasadVerma <vineetpd1996@gmail.com>
2019-09-29 11:58:21 +0000

    Request fakedata from jsonplaceholder

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,7 +1,9 @@
 var request = require("request");
-request("https://www.google.com", function(error, response, body){
+request("https://jsonplaceholder.typicode.com/users/", function(error, response, body){
 	if(!error && response.statusCode === 200){
-		console.log(body);
+		var parsedData = JSON.parse(body);
+		console.log(typeof body);
+		console.log(parsedData[0].name);
 	}else{
 		console.log(error);
 	}

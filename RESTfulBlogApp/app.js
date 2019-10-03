213b7d2386c2a788fab4d0a9fe61c2be7c1bfc3f
VineetPrasadVerma <vineetpd1996@gmail.com>
2019-10-03 10:33:35 +0000

    Add Update and Edit route to edit a blog

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -1,12 +1,14 @@
 var express    = require("express"),
 	app        = express(),
 	bodyParser = require("body-parser"),
-	mongoose   = require("mongoose");
+	mongoose   = require("mongoose"),
+	methodOverride = require("method-override");
 
-mongoose.connect("mongodb://localhost/restful_blog_app", {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/restful_blog_app", {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
 	title: String,
@@ -48,7 +50,40 @@ app.post("/blogs", (req, res) => {
 	});
 });
 
+//SHOW ROUTE
+app.get("/blogs/:id", (req, res) => {
+	Blog.findById(req.params.id, (err, foundBlog) => {
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("show", {blog: foundBlog});
+		}
+	});
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) => {
+	Blog.findById(req.params.id, (err, foundBlog) => {
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", (req, res) =>{
+	Blog.findOneAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs/"+req.params.id);
+		}
+	});
+});
 
 app.listen(3000, () => {
 	console.log("Blog server has started");
 });
+   

87c74d8e5ef934083cd092a689c834f72368ff9e
VineetPrasadVerma <vineetpd1996@gmail.com>
2019-10-03 05:35:27 +0000

    Add Create and New route to add a new blog via form

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -8,7 +8,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 var blogSchema = new mongoose.Schema({
 	title: String,
 	image: String,
@@ -22,12 +21,29 @@ app.get("/", (req, res) => {
 	res.redirect("/blogs");
 });
 
+//INDEX ROUTE
 app.get("/blogs", (req, res) => {
 	Blog.find({}, (err, blogs) => {
 		if(err){
 			console.log(err);
 		}else{
 			res.render("index", {blogs: blogs});
+		}
+	});
+});
+
+//NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+	res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", (req, res) => {
+	Blog.create(req.body.blog, (err, newBlog) => {
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect("/blogs");
 		}
 	});
 });

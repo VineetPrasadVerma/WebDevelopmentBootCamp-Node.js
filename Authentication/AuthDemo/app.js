6bc32866897944ac2efbf4ef7a2556e180581cce
VineetPrasadVerma <vineetpd1996@gmail.com>
2019-10-08 08:11:18 +0000

    Add Register,Login, Logout functionality by using passport module

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -10,7 +10,7 @@ passportLocalMongoose = require("passport-local-mongoose"),
 mongoose.connect("mongodb://localhost/auth_demo_app", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
 
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
 	secret:"Feleena is Love",
 	resave: false,
@@ -19,16 +19,68 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//=========
+//ROUTES
+//=========
 
 app.get("/", (req, res) => {
 	res.render("home");
 });
 
-app.get("/secret", (req, res) => {
+app.get("/secret", isLoggedin, (req, res) => {
 	res.render("secret");
 });
+
+//Auth Routes
+//Show signup form
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+//handling User sign up
+app.post("/register", (req,res) => {
+	var username = req.body.username;
+	var password = req.body.password;
+	
+	User.register(new User({username}), password, (err, user)=> {
+		if(err){
+			console.log(err);
+			return res.redirect("/register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/secret");
+		});
+		
+	});
+});
+
+//handling user login
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/secret", failureRedirect: "/login"}),
+	(req, res) => {
+	
+});
+
+//handling user logout
+app.get("/logout", (req, res) => {
+	req.logout();
+	res.redirect("/");
+});
+
+function isLoggedin(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 app.listen(3000, () => {
 	console.log("Server Started");
